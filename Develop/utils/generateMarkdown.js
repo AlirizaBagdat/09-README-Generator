9de2791d851c6977b9fe148e3d21f52f4c8626d8
Aliriza Bagdat <ambagdat@gmail.com>
2023-11-30 23:53:21 +0000

    write to file

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,75 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function to render a license badge
+function renderLicenseBadge(license) {
+  if (license) {
+    return ![License](https://img.shields.io/badge/license-${license}-brightgreen);
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function to render a license link
+function renderLicenseLink(license) {
+  if (license) {
+    return [License](https://opensource.org/licenses/${license});
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function to render the license section of README
+function renderLicenseSection(license) {
+  if (license) {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the [${license} License](${renderLicenseLink(license)}).`;
+  } else {
+    return '';
+  }
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+${data.license ? '- [License](#license)\n' : ''}
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions, please feel free to contact me:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+
+${renderLicenseSection(data.license)}
 `;
 }
 
